858d0795a617fa678b09ca4044aa44b5ab331757
mmensing <michael.mensing@studserv.uni-leipzig.de>
2015-06-09 09:21:52 +0200

    removed global vars

--- minion/minion_confocal.py
+++ minion/minion_confocal.py
@@ -19,11 +19,16 @@ from matplotlib.figure import Figure
 class MinionConfocalUi(QWidget):
     def __init__(self, parent=None):
         super(MinionConfocalUi, self).__init__(parent)
-
+        global settlingtime
+        settlingtime = 0.05
+        print(settlingtime)
         self.confocaltabs = QTabWidget()
         self.navigationtab = MinionConfocalNavigation()
+        self.navigationtab.uisetup()
         self.tilttab = MinionConfocalTilt()
         self.settingtab = MinionConfocalSetting()
+        self.settingtab.uisetup()
+        self.navigationtab.connectsettings()
 
         self.confocaltabs.addTab(self.navigationtab, 'Navigation')
         self.confocaltabs.addTab(self.tilttab, 'Tilt')
@@ -36,9 +41,10 @@ class MinionConfocalUi(QWidget):
 
 
 class MinionConfocalNavigation(QWidget):
-    def __init__(self, parent=None):
-        super(MinionConfocalNavigation, self).__init__(parent)
+    def __init__(self):
+        super(MinionConfocalNavigation, self).__init__()
 
+    def uisetup(self):
         # set and get initial variables
         # TODO - get these values from the hardware
         self.xmin = 0.0
@@ -53,11 +59,7 @@ class MinionConfocalNavigation(QWidget):
         self.mapdata = np.zeros((self.resolution, self.resolution))
         self.colormin = self.mapdata.min()
         self.colormax = self.mapdata.max()
-        # TODO - for loop to change mapdata values
 
-        self.uisetup()
-
-    def uisetup(self):
         # create map canvas
         self.mapfigure = Figure()
         self.mapcanvas = FigureCanvas(self.mapfigure)
@@ -281,7 +283,7 @@ class MinionConfocalNavigation(QWidget):
         self.mapcanvas.draw()
 
     def resolutiontextchanged(self):
-        print('resolutiontextchanged')
+        self.resolution = self.resolutiontext.value()
 
     def colormintextchanged(self):
         self.colormin = self.colormintext.value()
@@ -308,9 +310,13 @@ class MinionConfocalNavigation(QWidget):
         self.mapcanvas.draw()
 
     def mapstartclicked(self):
-
         print("[%s] start scan" % QThread.currentThread().objectName())
-        self.aquisition = MinionColfocalMapDataAquisition(self.resolution)
+        self.scanprogress.setRange(0, self.resolution-1)
+        self.scanprogress.setValue(0)
+
+        settlingtime = 0.001
+        counttime = 0.005
+        self.aquisition = MinionColfocalMapDataAquisition(self.resolution, settlingtime, counttime, self.xmin, self.xmax, self. ymin, self.ymax)
         self.confocalthread = QThread(self, objectName='workerThread')
         self.aquisition.moveToThread(self.confocalthread)
         self.aquisition.finished.connect(self.confocalthread.quit)
@@ -320,15 +326,25 @@ class MinionConfocalNavigation(QWidget):
         self.aquisition.update.connect(self.updatemap)
         self.confocalthread.start()
 
+    def connectsettings(self):
+        print('connect settings')
+        self.scansettings = MinionConfocalSetting()
+        self.scansettings.settingschanged.connect(self.updatesettings)
+
+    @pyqtSlot(float, float)
+    def updatesettings(self):
+        print('update settings')
+        print(self.settlingtime, self.counttime)
+
     @pyqtSlot(np.ndarray, int)
     def updatemap(self, mapdataupdate, col):
-        print("[%s] update" % QThread.currentThread().objectName())
+        # print("[%s] update" % QThread.currentThread().objectName())
         self.mapdata = mapdataupdate
-        # print(col)
         # start = time.time()
         self.map.set_data(self.mapdata)
         self.mapcanvas.draw()
         self.colorautoscalepress()
+        self.scanprogress.setValue(col)
         # print(time.time()-start)
 
     def mapstopclicked(self):
@@ -346,21 +362,53 @@ class MinionConfocalNavigation(QWidget):
 
 
 class MinionConfocalSetting(QWidget):
+    settingschanged = pyqtSignal(float, float)
+
     def __init__(self, parent=None):
         super(MinionConfocalSetting, self).__init__(parent)
 
-        self.open_confocal = QPushButton('open_confocal')
+    def uisetup(self):
+        # create elements
+        self.settlingtimelabel = QLabel('Settling Time [ms]')
+        self.settlingtimetext = QDoubleSpinBox()
+        self.settlingtimetext.setRange(0, 1000)
+        self.settlingtimetext.setValue(int(1*1000))
+        self.settlingtimetext.editingFinished.connect(self.timetextchanged)
+
+        self.counttimelabel = QLabel('Count Time [ms]')
+        self.counttimetext = QDoubleSpinBox()
+        self.counttimetext.setRange(0, 1000)
+        global counttime
+        self.counttimetext.setValue(int(5*1000))
+        self.counttimetext.editingFinished.connect(self.timetextchanged)
+
+        # navigationtab = MinionConfocalNavigation(self.settlingtime, self.counttime)
+        # self.settlingtimetext.editingFinished.connect(navigationtab.updatesettings)
+        # self.counttimetext.editingFinished.connect(navigationtab.updatesettings)
+
+        # create layout
         confocalsetting_layout = QGridLayout()
-        confocalsetting_layout.addWidget(self.open_confocal, 0, 0)
-        # confocalsetting_layout.setSpacing(1)
+        confocalsetting_layout.addWidget(self.settlingtimelabel, 0, 0)
+        confocalsetting_layout.addWidget(self.settlingtimetext, 0, 1)
+        confocalsetting_layout.addWidget(self.counttimelabel, 1, 0)
+        confocalsetting_layout.addWidget(self.counttimetext, 1, 1)
+        confocalsetting_layout.setSpacing(1)
         self.setLayout(confocalsetting_layout)
+
+    def timetextchanged(self):
+        settlingtime = self.settlingtimetext.value()/1000
+        counttime = self.counttimetext.value()/1000
+        print('changed:', settlingtime, counttime)
+        # self.settingschanged.emit(.settlingtime, self.counttime)
+        # MinionConfocalNavigation(self.settlingtime, self.counttime)
+
 
 
 class MinionConfocalTilt(QWidget):
     def __init__(self, parent=None):
         super(MinionConfocalTilt, self).__init__(parent)
 
-        self.open_confocal = QPushButton('open_confocal')
+        self.open_confocal = QPushButton('placeholder')
         confocalsetting_layout = QGridLayout()
         confocalsetting_layout.addWidget(self.open_confocal, 0, 0)
         # confocalsetting_layout.setSpacing(1)
@@ -372,32 +420,43 @@ class MinionColfocalMapDataAquisition(QObject):
     finished = pyqtSignal()
     update = pyqtSignal(np.ndarray, int)
 
-    def __init__(self, resolution):
+    def __init__(self, resolution, settlingtime, counttime, xmin, xmax, ymin, ymax):
         super(MinionColfocalMapDataAquisition, self).__init__()
         self.resolution = resolution
+        self.settlingtime = settlingtime
+        self.counttime = counttime
+        self.xmin = xmin
+        self.xmax = xmax
+        self.ymin = ymin
+        self.ymax = ymax
+
         self._isRunning = True
         print("[%s] create worker" % QThread.currentThread().objectName())
 
+    def stop(self):
+        self._isRunning=False
+
     def longrun(self):
-        resolution=self.resolution
-        mapdataupdate = np.zeros((resolution, resolution))
+        # resolution = self.resolution
+        mapdataupdate = np.zeros((self.resolution, self.resolution))
         print("[%s] start scan" % QThread.currentThread().objectName())
+        print('resolution', self.resolution)
 
         tstart = time.time()
-        for i in np.ndindex((resolution, resolution)):
+        for i in np.ndindex((self.resolution, self.resolution)):
             if not self._isRunning:
                 self.finished.emit()
             else:
                 # print("[%s]  loop" % QThread.currentThread().objectName())
-                time.sleep(0.001)  # wait
+                # print(self.settlingtime, self.counttime)
+                time.sleep(self.settlingtime)  # wait
 
-                time.sleep(0.005)  # measure
+                time.sleep(self.counttime)  # measure
 
                 self.value = np.random.randint(1, 1000)
 
                 mapdataupdate[i] += self.value
-                if i[1] == resolution-1:
-                    print('UPDATE', i[0])
+                if i[1] == self.resolution-1:
                     self.update.emit(mapdataupdate, i[0])
                 # print(time.time()-ttemp)
 
