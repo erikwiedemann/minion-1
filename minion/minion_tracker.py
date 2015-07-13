e4e3c16ef03097d8533fab00e8f1b0f10f797e3a
mmensing <michael.mensing@studserv.uni-leipzig.de>
2015-07-13 16:07:14 +0200

    thinking about ways of visualizing centertracker results

--- minion/minion_tracker.py
+++ minion/minion_tracker.py
@@ -22,10 +22,14 @@ from matplotlib.patches import Rectangle
 class MinionTrackerUI(QWidget):
     def __init__(self, parent=None):
         super(MinionTrackerUI, self).__init__(parent)
-        self.resolution1 = 51  # x
-        self.resolution2 = 51  # y
+        self.resolution1 = 11  # x
+        self.resolution2 = 11  # y
+        self.resolution3 = 11  # z
 
-        self.centerdata = np.zeros((self.resolution2, self.resolution1))
+        self.centerdataxy = np.zeros((self.resolution2, self.resolution1))
+        self.centerdataxz = np.zeros((self.resolution3, self.resolution1))
+        self.centerdatayz = np.zeros((self.resolution3, self.resolution2))
+
         self.mapdata = np.zeros((self.resolution2, self.resolution1))
         self.correlationdata = np.zeros((self.resolution2, self.resolution1))
         self.referencedata = np.zeros((self.resolution2, self.resolution1))
@@ -38,26 +42,67 @@ class MinionTrackerUI(QWidget):
     def uisetup(self):
         # CENTER TRACKER
         # xy canvas
-        self.centerfigure = Figure()
-        self.centercanvas = FigureCanvas(self.centerfigure)
-        self.centercanvas.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
-        self.centercanvas.setMinimumSize(50, 50)
-        self.centertoolbar = NavigationToolbar(self.centercanvas, self)
-        self.centeraxes = self.centerfigure.add_subplot(111)
-        self.centeraxes.hold(False)
+        self.centerfigurexy = Figure()
+        self.centercanvasxy = FigureCanvas(self.centerfigurexy)
+        self.centercanvasxy.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.centercanvasxy.setMinimumSize(50, 50)
+        self.centertoolbarxy = NavigationToolbar(self.centercanvasxy, self)
+        self.centeraxesxy = self.centerfigurexy.add_subplot(111)
+        self.centeraxesxy.hold(False)
 
-        self.centermap = self.centeraxes.matshow(self.centerdata, origin='lower', extent=[0, self.resolution1, 0, self.resolution2])
-        self.centercolorbar = self.centerfigure.colorbar(self.centermap, fraction=0.046, pad=0.04, cmap=mpl.cm.jet)
-        self.centercolorbar.formatter.set_scientific(True)
-        self.centercolorbar.formatter.set_powerlimits((0, 3))
-        self.centercolorbar.update_ticks()
-        self.centeraxes.xaxis.set_ticks_position('bottom')
-        self.centeraxes.xaxis.set_tick_params(direction='out')
-        self.centeraxes.yaxis.set_ticks_position('left')
-        self.centeraxes.yaxis.set_tick_params(direction='out')
+        self.centermapxy = self.centeraxesxy.matshow(self.centerdataxy, origin='lower', extent=[0, self.resolution1, 0, self.resolution2])
+        self.centeraxesxy.set_xlabel('xy')
+        self.centeraxesxy.xaxis.set_label_position('top')
+        self.centercolorbarxy = self.centerfigurexy.colorbar(self.centermapxy, fraction=0.046, pad=0.04, cmap=mpl.cm.jet)
+        self.centercolorbarxy.formatter.set_scientific(True)
+        self.centercolorbarxy.formatter.set_powerlimits((0, 3))
+        self.centercolorbarxy.update_ticks()
+        self.centeraxesxy.xaxis.set_ticks_position('bottom')
+        self.centeraxesxy.xaxis.set_tick_params(direction='out')
+        self.centeraxesxy.yaxis.set_ticks_position('left')
+        self.centeraxesxy.yaxis.set_tick_params(direction='out')
 
+        # xz canvas
+        self.centerfigurexz = Figure()
+        self.centercanvasxz = FigureCanvas(self.centerfigurexz)
+        self.centercanvasxz.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.centercanvasxz.setMinimumSize(50, 50)
+        self.centertoolbarxz = NavigationToolbar(self.centercanvasxz, self)
+        self.centeraxesxz = self.centerfigurexz.add_subplot(111)
+        self.centeraxesxz.hold(False)
 
+        self.centermapxz = self.centeraxesxz.matshow(self.centerdataxz, origin='lower', extent=[0, self.resolution1, 0, self.resolution3])
+        self.centeraxesxz.set_xlabel('xz')
+        self.centeraxesxz.xaxis.set_label_position('top')
+        self.centercolorbarxz = self.centerfigurexz.colorbar(self.centermapxz, fraction=0.046, pad=0.04, cmap=mpl.cm.jet)
+        self.centercolorbarxz.formatter.set_scientific(True)
+        self.centercolorbarxz.formatter.set_powerlimits((0, 3))
+        self.centercolorbarxz.update_ticks()
+        self.centeraxesxz.xaxis.set_ticks_position('bottom')
+        self.centeraxesxz.xaxis.set_tick_params(direction='out')
+        self.centeraxesxz.yaxis.set_ticks_position('left')
+        self.centeraxesxz.yaxis.set_tick_params(direction='out')
 
+        # yz canvas
+        self.centerfigureyz = Figure()
+        self.centercanvasyz = FigureCanvas(self.centerfigureyz)
+        self.centercanvasyz.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
+        self.centercanvasyz.setMinimumSize(50, 50)
+        self.centertoolbaryz = NavigationToolbar(self.centercanvasyz, self)
+        self.centeraxesyz = self.centerfigureyz.add_subplot(111)
+        self.centeraxesyz.hold(False)
+
+        self.centermapyz = self.centeraxesyz.matshow(self.centerdatayz, origin='lower', extent=[0, self.resolution2, 0, self.resolution3])
+        self.centeraxesyz.set_xlabel('yz')
+        self.centeraxesyz.xaxis.set_label_position('top')
+        self.centercolorbaryz = self.centerfigureyz.colorbar(self.centermapyz, fraction=0.046, pad=0.04, cmap=mpl.cm.jet)
+        self.centercolorbaryz.formatter.set_scientific(True)
+        self.centercolorbaryz.formatter.set_powerlimits((0, 3))
+        self.centercolorbaryz.update_ticks()
+        self.centeraxesyz.xaxis.set_ticks_position('bottom')
+        self.centeraxesyz.xaxis.set_tick_params(direction='out')
+        self.centeraxesyz.yaxis.set_ticks_position('left')
+        self.centeraxesyz.yaxis.set_tick_params(direction='out')
 
 
         self.button = QPushButton('banane')
@@ -159,23 +204,30 @@ class MinionTrackerUI(QWidget):
         # -------------------------------------------------------------------------------------------------------------
         # centertracker
         centertrackertablayout = QGridLayout()
-        centertrackertablayout.addWidget(self.centercanvas, 0, 0, 10, 10)
-        centertrackertablayout.addWidget(self.centertoolbar, 10, 0, 1, 10)
+        centertrackerplotbox = QVBoxLayout()
+        centertrackerplotbox.addWidget(self.centercanvasxy)
+        centertrackerplotbox.addWidget(self.centertoolbarxy)
+        centertrackerplotbox.addWidget(self.centercanvasxz)
+        centertrackerplotbox.addWidget(self.centertoolbarxz)
+        centertrackerplotbox.addWidget(self.centercanvasyz)
+        centertrackerplotbox.addWidget(self.centertoolbaryz)
+        centertrackertablayout.addLayout(centertrackerplotbox, 0, 0)
 
 
-
-        centertrackertablayout.addWidget(self.button)
+        centertrackertablayout.addWidget(self.button, 0, 1)
 
         # -------------------------------------------------------------------------------------------------------------
         # maptracker
         maptrackertablayout = QGridLayout()
-        maptrackertablayout.setSpacing(2)
+
+
         maptrackertablayout.addWidget(self.mapcanvas, 0, 0, 10, 10)
         maptrackertablayout.addWidget(self.maptoolbar, 10, 0, 1, 10)
-        maptrackertablayout.addWidget(self.referencecanvas, 0, 20, 10, 10)
-        maptrackertablayout.addWidget(self.referencetoolbar, 10, 20, 1, 10)
         maptrackertablayout.addWidget(self.correlationcanvas, 0, 10, 10, 10)
         maptrackertablayout.addWidget(self.correlationtoolbar, 10, 10, 1, 10)
+        maptrackertablayout.addWidget(self.referencecanvas, 0, 20, 10, 10)
+        maptrackertablayout.addWidget(self.referencetoolbar, 10, 20, 1, 10)
+
         maptrackertablayout.addWidget(self.contexttrackertable, 11, 0, 10, 10)
         maptrackertablayout.addWidget(self.loadlastscanbutton, 11, 10)
         maptrackertablayout.addWidget(self.clearreferencebutton, 12, 10)
